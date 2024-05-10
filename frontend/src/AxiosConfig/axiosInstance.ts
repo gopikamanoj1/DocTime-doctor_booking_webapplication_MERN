d2404a247c6879ea88e29896760685595d8bebb0
gopika <gopikamanoj001@gmail.com>
2024-05-10 21:23:30 +0530

    the localhost change to domain

--- frontend/src/AxiosConfig/axiosInstance.ts
+++ frontend/src/AxiosConfig/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default settings
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://doctime.live",
   timeout: 100000, // 10 seconds timeout
   headers: {
     'Content-Type': 'application/json',
