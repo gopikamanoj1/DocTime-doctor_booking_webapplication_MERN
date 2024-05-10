5858e015d66b2cf8a693a167f54af80f0eebc5d3
gopika <gopikamanoj001@gmail.com>
2024-05-10 19:23:19 +0530

    mongo db url changed to atlas

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -11,6 +11,7 @@
 export default {
   port: process.env.PORT || 3000,
   mongo: {
-    uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/DocTime',
+    uri: 'mongodb+srv://gopikamanoj001:Gopika%40123@doctime.eyvgrkj.mongodb.net/doctime?retryWrites=true&w=majority&appName=doctime',
   },
 };
+   
