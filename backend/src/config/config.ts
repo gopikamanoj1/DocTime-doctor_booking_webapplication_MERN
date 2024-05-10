e148bae6ebe607a529eba1ea6b0f61cb3cfe7ca2
gopika <gopikamanoj001@gmail.com>
2024-05-10 19:48:40 +0530

    some changes in mongo config file

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,12 +1,3 @@
-// export default {
-//     port :3000,
-//     mongo:{
-//         uri:'mongodb://127.0.0.1:27017/DocTime'
-
-//     }
-// }
-
-
 
 export default {
   port: process.env.PORT || 3000,
