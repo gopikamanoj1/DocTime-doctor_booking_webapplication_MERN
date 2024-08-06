2a30481d0ae378b4134eba5495d25d8878c270b3
Gopika Manoj <138016356+gopikamanoj1@users.noreply.github.com>
2024-08-06 11:12:25 +0530

    socket url changed

--- frontend/src/REAL_TIME/Socket.tsx
+++ frontend/src/REAL_TIME/Socket.tsx
@@ -6,7 +6,7 @@ export const useSocket = (): Socket<any>| null => {
     const [socket, setSocket] = useState<Socket<any> | null>(null);
 
     useEffect(() => {
-        const newSocket = io("http://localhost:3000", {
+        const newSocket = io("https://doctime-doctor-booking-webapplication-42qm.onrender.com", {
             reconnect: true,
             secure: true,
             transports: ['polling', 'websocket'], 
