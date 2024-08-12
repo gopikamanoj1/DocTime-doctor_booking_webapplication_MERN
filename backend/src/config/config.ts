d9a8674727c7bf28be2f9d82b6ef5dc8646a3bea
Gopika Manoj <138016356+gopikamanoj1@users.noreply.github.com>
2024-08-12 20:40:27 +0530

    port err fixed

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 export default {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   mongo: {
     uri: 'mongodb+srv://gopikamanoj001:Gopika%40123@doctime.eyvgrkj.mongodb.net/doctime?retryWrites=true&w=majority&appName=doctime',
   },
