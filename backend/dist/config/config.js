d8829f391f59cb436420d66ba7d44ee5986d00d3
Gopika Manoj <138016356+gopikamanoj1@users.noreply.github.com>
2024-08-12 20:27:46 +0530

    port changed

--- backend/dist/config/config.js
+++ backend/dist/config/config.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.default = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
     mongo: {
         uri: 'mongodb+srv://gopikamanoj001:Gopika%40123@doctime.eyvgrkj.mongodb.net/doctime?retryWrites=true&w=majority&appName=doctime',
     },
