5a0f89db4d7523f60a4071a9db42479cec8423e3
Gopika Manoj <138016356+gopikamanoj1@users.noreply.github.com>
2024-08-13 02:29:42 +0530

    axios

--- frontend/src/AxiosConfig/axiosInstance.ts
+++ frontend/src/AxiosConfig/axiosInstance.ts
@@ -2,14 +2,14 @@
 import axios from 'axios';
 
 // Create an Axios instance with default settings
-// const axiosInstance = axios.create({
-//   baseURL: "/api",
-//   timeout: 100000, // 10 seconds timeout
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   withCredentials: true, 
-// });
+const axiosInstance = axios.create({
+  baseURL: "/api",
+  timeout: 100000, // 10 seconds timeout
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true, 
+});
 
 
 // const axiosInstance = axios.create({
@@ -20,20 +20,15 @@ import axios from 'axios';
 //   },
 //   withCredentials: true, 
 // });
-// export default axiosInstance;
 
 
 
-// axios.js
 
-const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? "https://doctime.live" : "/api",
-  timeout: 30000, // 30 seconds timeout
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  withCredentials: true,
-});
 
 export default axiosInstance;
+
+
+
+
+
 
